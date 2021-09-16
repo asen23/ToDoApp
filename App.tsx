d9bc6069b9f505557d1f937471c13d9d95d3193b
asen23 <andersenlim23@gmail.com>
2021-09-16 13:18:23 +0700

    ref: removed unused style and import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
@@ -18,12 +17,3 @@ export default function App() {
         </SafeAreaProvider>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-})
