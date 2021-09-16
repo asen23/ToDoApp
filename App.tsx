461eb7e7db2178549f27ea5a8de6f161af91970e
asen23 <andersenlim23@gmail.com>
2021-09-16 13:17:55 +0700

    feat: add stack navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import HomeScreen from "./screens/HomeScreen"
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
     return (
         <SafeAreaProvider>
-            <View style={styles.container}>
-                <Text>Open up App.tsx to start working on your app!</Text>
-                <StatusBar style="auto" />
-            </View>
-            <StatusBar />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
         </SafeAreaProvider>
     )
 }
