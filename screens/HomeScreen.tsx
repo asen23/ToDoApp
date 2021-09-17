e17e9f7b84c15f7f983218ed0bbabf38edfd27fa
asen23 <andersenlim23@gmail.com>
2021-09-17 21:31:08 +0700

    feat: changed homescreen to todo

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,19 +1,53 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { View, TextInput, TouchableOpacity, FlatList, Text } from "react-native"
+import tw from "tailwind"
+import { MaterialIcons } from "@expo/vector-icons"
+import { Todo } from "type"
 
 export default function HomeScreen() {
+    const [text, setText] = useState("")
+    const [inputIsActive, setInputIsActive] = useState(false)
+    const [todo, setTodo] = useState<Todo[]>([])
+
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+        <View
+            // class="
+            style={tw`flex-1 items-stretch m-2`}
+            // "
+        >
+            <View
+                // class="
+                style={tw`flex-row p-2 border border-black rounded-3xl`}
+                // "
+            >
+                <TextInput
+                    onChangeText={setText}
+                    value={text}
+                    placeholder="Add todo"
+                    onBlur={() => setInputIsActive(false)}
+                    onFocus={() => setInputIsActive(true)}
+                    // class="
+                    style={tw.style("flex-1", {
+                        "border-transparent": inputIsActive,
+                    })}
+                    // "
+                />
+                <TouchableOpacity
+                    onPress={() => {
+                        setTodo((todo) => [
+                            ...todo,
+                            { completed: false, todo: text },
+                        ])
+                        setText("")
+                    }}
+                >
+                    <MaterialIcons name="add" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                data={todo}
+                renderItem={({ item }) => <Text>{item.todo}</Text>}
+            />
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-})
