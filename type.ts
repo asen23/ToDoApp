9975c6b2a54c850135701f6983d8613a697a42f0
asen23 <andersenlim23@gmail.com>
2021-09-19 13:54:54 +0700

    feat: add todoprop validator

--- type.ts
+++ type.ts
@@ -1,4 +1,5 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import PropTypes from 'prop-types'
 
 export type StackParamList = {
     "To Do": undefined,
@@ -10,4 +11,10 @@ export type Todo = {
     id: string,
     completed: boolean,
     todo: string,
+}
+
+export var TodoProp = {
+    id: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+    todo: PropTypes.string.isRequired,
 }
