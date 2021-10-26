857cf367e2912568a271bd85d6a5c21d57787a48
asen23 <andersenlim23@gmail.com>
2021-10-26 08:45:35 +0700

    ref: cleanup code

--- screens/ToDo/ToDoItem/ToDoItem.tsx
+++ screens/ToDo/ToDoItem/ToDoItem.tsx
@@ -1,19 +1,21 @@
+import React from 'react';
 import PropTypes, { InferProps } from "prop-types"
 import { TodoProp } from "type"
 import tw from "tailwind"
 import { ListItem, Button } from "react-native-elements"
 
-function ToDoItem({ item, onRemove, onChangeCompleted }: InferProps<typeof ToDoItem.propTypes>) {
-
+function ToDoItem({
+    item,
+    onRemove,
+    onChangeCompleted,
+}: InferProps<typeof ToDoItem.propTypes>) {
     return (
         <ListItem.Swipeable
             key={item.id}
             rightContent={
                 <Button
                     icon={{ name: "delete", color: "white" }}
-                    // class="
                     buttonStyle={tw`bg-red-600 h-full w-full`}
-                    // "
                     onPress={() => {
                         onRemove(item.id)
                     }}
@@ -22,18 +24,14 @@ function ToDoItem({ item, onRemove, onChangeCompleted }: InferProps<typeof ToDoI
             bottomDivider
         >
             <ListItem.Content
-                // class="
                 style={tw`flex-row justify-start`}
-                // "
             >
                 <ListItem.CheckBox
                     checked={item.completed}
                     onPress={() => onChangeCompleted(item.id)}
                 />
                 <ListItem.Title
-                    // class="
                     style={tw.style({ "line-through": item.completed })}
-                    // "
                 >
                     {item.todo}
                 </ListItem.Title>
--- screens/ToDo/ToDoScreen.tsx
+++ screens/ToDo/ToDoScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { View, TextInput, TouchableOpacity, FlatList, Text } from "react-native"
+import { View, TextInput, TouchableOpacity, FlatList } from "react-native"
 import tw from "tailwind"
 import { MaterialIcons } from "@expo/vector-icons"
 import { v4 as uuid } from "uuid"
@@ -39,23 +39,17 @@ export default function ToDoScreen() {
 
     return (
         <View
-            // class="
             style={tw`flex-1 items-stretch m-2`}
-            // "
         >
             <View
-                // class="
                 style={tw`flex-row p-2 border border-black rounded-3xl bg-white`}
-                // "
             >
                 <TextInput
                     onChangeText={setText}
                     onSubmitEditing={addTodo}
                     value={text}
                     placeholder="Add todo"
-                    // class="
                     style={tw`flex-1`}
-                    // "
                 />
                 <TouchableOpacity onPress={addTodo}>
                     <MaterialIcons name="add" size={24} color="black" />
